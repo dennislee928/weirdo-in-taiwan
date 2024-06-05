0aa428de01ade50bbffc70d6242cf2305d375816
Bityacht-Developer <55692615+dennislee928@users.noreply.github.com>
2024-06-05 13:31:06 +0800

    initiallize

---
+++ weirdo_in_taiwan/weirdo_in_taiwan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for weirdo_in_taiwan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weirdo_in_taiwan.settings')
+
+application = get_asgi_application()
